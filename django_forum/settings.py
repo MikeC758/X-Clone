d0af44496c33689ce9aa7d4f827526e96928b769
MikeC758 <168582664+MikeC758@users.noreply.github.com>
2024-09-17 22:46:06 -0500

    Update settings.py

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = 'static/'
 STATICFILES_DIRS =[
